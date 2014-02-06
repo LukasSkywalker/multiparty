eb2e84861df18376e5254a2f9dac7c33180cbf27
Lukas_Skywalker <lukas.diener@hotmail.com>
2014-02-07 00:08:05 +0100

    Initial commit

---
+++ multiparty.js
@@ -0,0 +1,30 @@
+function MultiParty(boundary) {
+  if(boundary)
+    this.boundary = boundary;
+  else
+    this.boundary = '---------------------------577900546148291341889429421';
+
+  this.body = 'Content-Type: multipart/form-data; boundary=' + this.boundary + '\r\n';
+  this.body += 'Content-Length: 81602\r\n\r\n';
+}
+
+MultiParty.prototype.addParam = function(name, value) {
+  this.body += '--' + this.boundary + '\r\n';
+  this.body += 'Content-Disposition: form-data; name="' + name + '"\r\n\r\n';
+  this.body += value + '\r\n';
+};
+
+MultiParty.prototype.addFile = function(name, fileName, contentType, content) {
+  this.body += '--' + this.boundary + '\r\n';
+  this.body += 'Content-Disposition: form-data; name="'+ name + '"; filename="' + fileName + '"\r\n';
+  this.body += 'Content-Type: ' + contentType + '\r\n\r\n';
+  this.body += content + '\r\n';
+};
+
+MultiParty.prototype.getBody = function() {
+  return this.body + '--' + this.boundary + '--\r\n';
+};
+
+MultiParty.prototype.getContentTypeHeader = function() {
+  return "multipart/form-data; boundary=" + this.boundary;
+}
